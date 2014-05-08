0cd9d8e0b5faee43f4630446a09daa65529335c4
emak <emilien.ak@gmail.com>
2014-05-08 19:38:58 +0200

    Polish index layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Adoptajerry::Application.routes.draw do
+  get "/start" => 'pages#start'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
