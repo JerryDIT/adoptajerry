cb3c129733af71b8c2cba9bb1aa8bf5766518233
emak <emilien.ak@gmail.com>
2015-09-13 17:18:58 +0200

    Clean update action
    * extract maker clearance check
    * extract the update of team members
    * prepare the call to UpdateJerry interactor

--- app/controllers/jerries_controller.rb
+++ app/controllers/jerries_controller.rb
@@ -3,6 +3,7 @@ require Rails.root.join('app/interactions/jerries/create_jerry')
 class JerriesController < ApplicationController
   before_action :find_jerry, except: [:new, :create, :index, :roulette]
   before_action :maker_authenticate, except: [:index, :show, :roulette]
+  before_action :check_maker_clearance, only: [:edit, :update, :edit_team]
 
   def new
     @jerry = Jerry.new
@@ -30,37 +31,25 @@ class JerriesController < ApplicationController
   end
 
   def edit
-    redirect_to @jerry, notice: 'You can not edit this Jerry' unless maker_allowed?
   end
 
   def update
-    if maker_allowed?
-      if params[:jerry][:makers]
-        new_maker = Maker.find_by :uid => params[:jerry][:makers][:uid]
-        if new_maker
-          @jerry.makers << new_maker
-        else
-          flash.now.notice = "Unknown maker '#{params[:jerry][:makers][:uid]}', can not be added to the team"
-          return render 'edit_team'
-        end
-      end
-      if @jerry.update jerry_params
-        redirect_to @jerry
-      else
-        render 'edit'
-      end
+    update_team
+    outcome = UpdateJerry.run(params: params[:jerry],
+                              images: params[:jerry][:pictures][:image])
+    if outcome.valid?
+      redirect_to jerry_path outcome.result
     else
-      redirect_to @jerry, notice: 'You can not edit this Jerry'
+      render :edit
     end
-  end
-
-  def edit_team
-    redirect_to @jerry, notice: 'You can not edit this Jerry' unless maker_allowed?
   end
 
   def destroy
     @jerry.destroy
     redirect_to jerries_path
+  end
+
+  def edit_team
   end
 
   def roulette
@@ -79,8 +68,24 @@ class JerriesController < ApplicationController
     redirect_to root_path, notice: 'Please Sign in first --→' unless current_maker
   end
 
-  def maker_allowed?
-    @jerry.makers.include? current_maker
+  def check_maker_clearance
+    if @jerry.makers.include? current_maker
+      # all right you can continue
+    else
+      redirect_to @jerry, notice: 'You can not edit this Jerry'
+    end
+  end
+
+  def update_team
+    if params[:jerry][:makers]
+      new_maker = Maker.find_by :uid => params[:jerry][:makers][:uid]
+      if new_maker
+        @jerry.makers << new_maker
+      else
+        flash.now.notice = "Unknown maker '#{params[:jerry][:makers][:uid]}', can not be added to the team"
+        return render 'edit_team'
+      end
+    end
   end
 
   def jerry_params
