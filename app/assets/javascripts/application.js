411f2b9b308b5795e76d49993098216e4021f1c2
Sébastien Bourdu <git@ekkans.com>
2015-09-17 01:22:26 +0200

    Refactored js architecture of files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,26 +53,9 @@
 //= require api-check
 //= require angular-formly
 //= require angular-formly-templates-bootstrap
+//=
+//= require smooth-anchor-scrolling
+//= require services/persona
 //
 //= require app/init
 //= require_tree ./app
-//=
-//= require smooth-anchor-scrolling
-
-function loginViaEmail() {
-  navigator.id.get(function(assertion) {
-    if (assertion) {
-      $('input[name=assertion]').val(assertion);
-      $('#browser_id_form').submit();
-    } else {
-      window.location = "#{failure_path}"
-    }
-  });
-}
-
-$(function() {
-  $('#browser_id_form button').click(function() {
-    loginViaEmail();
-    return false;
-  });
-});
